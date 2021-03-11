9e4670c528c6d76927e06680d69004d57691d116
Josh <github@fayer.me>
2021-03-10 19:00:19 -0500

    v1.0: queues, url updates, right-click menu, bug fixes

--- index.js
+++ index.js
@@ -9,30 +9,8 @@ const app = express();
 app.use(cors());
 app.use(express.static(__dirname + '/build'));
 
-var xmlTree;
-var names;
-
-fsPromises.readFile('./tree.xml')
-.then(res => res.toString())
-.then(text => {
-
-    const xml = xmlParser(text, (err,res) => {
-        xmlTree = res;
-        let files = xmlTree['files']['file'];
-        let oggs = files.filter(i => i['$']['name'].includes('.ogg'));
-        names = oggs.map(i => i['$']['name']);
-        
-    })
-
-})
-
-app.get('/', (req, res) => {
+app.get('/*', (req, res) => {
     res.sendFile(path.join(__dirname) + '/build/index.html');
 });
 
-app.get('/files', (req,res) => {
-
-    res.send(names);
-})
-
-app.listen(3000);
+app.listen(80);
